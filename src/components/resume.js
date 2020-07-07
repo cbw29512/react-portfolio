4cd98dbe0dd81c2ac675825e63a3242b9fa33909
cbw29512 <divclass01@gmail.com>
2020-07-07 09:59:19 -0400

    minor changes

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,9 +4,12 @@ import Education from './education';
 import Experience from './experience';
 import Skills from './skills';
 
+
+
 class Resume extends Component {
   render() {
     return(
+      
       <div>
         <Grid>
           <Cell col={4}>
@@ -148,7 +151,7 @@ and functionality for use by web applications.</p>
           </Cell>
         </Grid>
       </div>
-  
+      
     )
   }
 }
