1de2e8aa42defc12a12c028d54db3f360213fa5a
Chris Wilson <divclass01@gmail.com>
2026-07-01 13:27:57 -0400

    Remove resume date rendering

--- src/components/education.js
+++ src/components/education.js
@@ -3,17 +3,9 @@ import { Grid, Cell } from 'react-mdl';
 
 class Education extends Component {
   render() {
-    const hasDate = this.props.startYear || this.props.endYear;
-    const dateText = [this.props.startYear, this.props.endYear].filter(Boolean).join(' - ');
-
     return(
       <Grid className="resume-item">
-        {hasDate && (
-          <Cell col={4}>
-            <p>{dateText}</p>
-          </Cell>
-        )}
-        <Cell col={hasDate ? 8 : 12}>
+        <Cell col={12}>
           <h4>{this.props.schoolDescription}</h4>
           <h6 style={{marginTop:'0px'}}>{this.props.schoolName}</h6>
         </Cell>
