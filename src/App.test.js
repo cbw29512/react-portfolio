fe732c5169b1d02018884aa17e0b934cb7c9ad87
Chris Wilson <divclass01@gmail.com>
2026-09-15 12:05:44 -0400

    Make app smoke test exercise router context

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders the application shell without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+  try {
+    ReactDOM.render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+      div
+    );
+  } finally {
+    ReactDOM.unmountComponentAtNode(div);
+  }
 });
