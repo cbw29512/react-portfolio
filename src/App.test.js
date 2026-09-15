442f245747b39ad1209a68ba04788cfaf8a80d30
Chris Wilson <divclass01@gmail.com>
2026-09-15 12:06:03 -0400

    Cover every portfolio route in smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders the application shell without crashing', () => {
-  const div = document.createElement('div');
+const ROUTES = ['/', '/projects', '/resume', '/contact', '/missing-route'];
 
-  try {
-    ReactDOM.render(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>,
-      div
-    );
-  } finally {
-    ReactDOM.unmountComponentAtNode(div);
-  }
+ROUTES.forEach((route) => {
+  it(`renders ${route} without crashing`, () => {
+    const div = document.createElement('div');
+
+    try {
+      ReactDOM.render(
+        <MemoryRouter initialEntries={[route]}>
+          <App />
+        </MemoryRouter>,
+        div
+      );
+    } finally {
+      ReactDOM.unmountComponentAtNode(div);
+    }
+  });
 });
